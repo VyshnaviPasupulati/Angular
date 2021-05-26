2128e96c3b7de1c81151076e7a284c28a948d37d
VyshnaviPasupulati <pmunivyshnavi@gmail.com>
2021-05-26 20:29:16 +0530

    Applications using Angular

---
+++ login-app/src/app/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { LoginModel } from 'src/login.model';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  auth : LoginModel;
+  flag : boolean;
+
+  constructor() { 
+    this.auth = new LoginModel();
+  }
+
+  ngOnInit(): void {
+  }
+  validate(){
+    console.log(this.auth.userid + " = " +this.auth.passwd);
+    if(this.auth.userid=='milak' && this.auth.passwd=='milashton')
+     this.flag = true;
+  }
+
+}
